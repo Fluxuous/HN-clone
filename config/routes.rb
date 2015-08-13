dbeba75af8bcddc9dd7a1ca8537e9ccd759578bf
Parker Ferguson <parkerferguson@gmail.com>
2015-08-13 15:39:32 -0400

    adds controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
 	root to: 'application#angular'
+
+
+	resources :posts, only: [:create, :index, :show] do
+	  resources :comments, only: [:show, :create] do
+	  	member do
+	  		put '/upvote' => 'comments#upvote'
+	  	end
+	  end
+
+	  member do
+	  	put '/upvote' => 'posts#upvote'
+	  end
+	end
 end
